b8078688fc01525cdd66e174a7f06359b147f3ba
jhay <jhay.alcorcon@gmail.com>
2025-09-24 02:16:22 +0800

    minor fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 export { Landing } from './Landing';
 export { default as Login } from './Login';
 export { default as NotFound } from './NotFound';
+export { default as Register } from './Register';
 export { default as Room } from './Room';
 export { default as RoomDetailsPage } from './RoomDetailsPage';
